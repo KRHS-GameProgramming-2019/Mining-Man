a00779521eac965bc5c058a99cc00d5f6b6505dd
RoxanneChambers2022 <roxanne.chambers@kearsarge.org>
2019-12-10 11:25:49 -0500

    i started with making the ore and loading the image

--- Ore.py
+++ Ore.py
@@ -1,1 +1,11 @@
+from math import *
+from pygame import*
+
+class Ore():
+     def __init__(self, image, pos):
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()\
+        
+    
+    
 
