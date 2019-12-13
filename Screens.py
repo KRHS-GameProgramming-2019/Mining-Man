94b07f68ed6f27f7b05f7dc59511c03bc1cdcd03
kgoodwin1 <kyle.goodwin@kearsarge.org>
2019-12-13 11:33:40 -0500

    title screen!!!!!

--- Screens.py
+++ Screens.py
@@ -4,3 +4,9 @@ def Titlescreen(debug=False):
     backgroundImage = (r"C:\Users\Student\Documents\Game Programming\Mining Man\Images\TitleScreen\titlescreenbackground.png")
     
     
+def options(debug=False):
+    if debug: print("TitleScreen Function")
+    
+    backgroundImage = (r"C:\Users\Student\Documents\Game Programming\Mining Man\Images\TitleScreen\titlescreenbackground.png")
+    
+    
