421a44c531f7d73086311b894c1378e41af19f04
RoxanneChambers2022 <roxanne.chambers@kearsarge.org>
2020-01-08 10:52:40 -0500

    umm

--- practice.py
+++ practice.py
@@ -21,8 +21,7 @@ Guy = Guy()
 while True:
     for event in pygame.event.get():
         coal = pygame.image.load("images/ores/coal.png")
-        image2
-
+        imgRect = image.get_rect()
         if event.type == pygame.QUIT:
             sys.exit();
         elif event.type == pygame.MOUSEBUTTONDOWN:
